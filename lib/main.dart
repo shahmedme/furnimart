fe31a6cb6903bcd97eaa2027ccdd3c057679abaf
shakilahmmeed <shakilahmmeed@gmail.com>
2020-03-18 00:36:00 +0600

    homepage complete

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
             Stack(
               children: <Widget>[
                 Container(
-                  height: 250.0,
+                  height: MediaQuery.of(context).size.height / 2.9,
                   width: double.infinity,
                   color: Color.fromARGB(255, 254, 215, 78),
                 ),
@@ -66,8 +66,7 @@ class HomePage extends StatelessWidget {
                                         style: BorderStyle.solid),
                                     borderRadius: BorderRadius.circular(30.0),
                                     image: DecorationImage(
-                                        image:
-                                        AssetImage('images/chris.jpg')),
+                                        image: AssetImage('images/shakil.jpg')),
                                   )),
                               Container(
                                 child: IconButton(
@@ -82,7 +81,7 @@ class HomePage extends StatelessWidget {
                         Container(
                           margin: EdgeInsets.only(top: 17.0),
                           child: Text(
-                            'Hello, Pino',
+                            'Hello, Shakil',
                             textAlign: TextAlign.start,
                             style: TextStyle(
                                 fontFamily: 'Poppins',
@@ -125,34 +124,204 @@ class HomePage extends StatelessWidget {
               ],
             ),
             SizedBox(
-              height: 20.0,
+              height: 15.0,
             ),
-            Stack(
-              children: <Widget>[
-                Material(
-                  elevation: 1.0,
-                  child: Container(
-                    height: 75.0,
-                    color: Colors.white,
-                  ),
-                ),
-                Row(
+            Material(
+              elevation: 1.0,
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.only(top: 10.0, bottom: 6.0),
+                child: Row(
                   children: <Widget>[
                     Container(
                       height: 75.0,
-                      width: MediaQuery.of(context).size.width/4,
+                      width: MediaQuery.of(context).size.width / 4,
                       child: Column(
                         children: <Widget>[
-                          Image(image: AssetImage('images/sofa.png'),),
-                          Text('Sofas')
+                          Container(
+                            height: 50.0,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('images/sofas.png'))),
+                          ),
+                          Text(
+                            'Sofas',
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 75.0,
+                      width: MediaQuery.of(context).size.width / 4,
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 50.0,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('images/wardrobe.png'))),
+                          ),
+                          Text(
+                            'Wardrobe',
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 75.0,
+                      width: MediaQuery.of(context).size.width / 4,
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 50.0,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('images/desks.png'))),
+                          ),
+                          Text(
+                            'Desk',
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 75.0,
+                      width: MediaQuery.of(context).size.width / 4,
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 50.0,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage('images/dressers.png'))),
+                          ),
+                          Text(
+                            'Dresser',
+                            style: TextStyle(fontFamily: 'Poppins'),
+                          )
                         ],
                       ),
                     )
                   ],
-                )
-              ],
-            )
+                ),
+              ),
+            ),
+            itemCard('FinnNavian', 'images/ottoman.jpg', true),
+            itemCard('FinnNavian', 'images/chair.jpg', false),
+            itemCard('FinnNavian', 'images/anotherchair.jpg', true),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget itemCard(String title, String imgPath, bool isFavourite) {
+    return Padding(
+      padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 20.0),
+      child: Material(
+        elevation: 2.0,
+        child: Container(
+          width: double.infinity,
+          color: Colors.white,
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 140.0,
+                height: 150.0,
+                margin: EdgeInsets.only(left: 10.0),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(imgPath), fit: BoxFit.contain)),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Poppins',
+                                fontSize: 20.0),
+                          ),
+                          Material(
+                            elevation: 2.0,
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Container(
+                              width: 40.0,
+                              height: 40.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: isFavourite
+                                      ? Colors.white
+                                      : Colors.grey.withOpacity(0.2)),
+                              child: isFavourite
+                                  ? Padding(
+                                      padding: EdgeInsets.only(top: 6.0),
+                                      child: Icon(
+                                        Icons.favorite,
+                                        color: Colors.red,
+                                      ),
+                                    )
+                                  : Padding(
+                                      padding: EdgeInsets.only(top: 5.0),
+                                      child: Icon(Icons.favorite_border)),
+                            ),
+                          )
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 10.0),
+                        child: Text(
+                            'Scandinavian small sized double sofa imported full leather / Dale italia oil.'),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        margin: EdgeInsets.only(top: 15.0),
+                        color: Color.fromARGB(255, 255, 221, 91),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              color: Color.fromARGB(255, 253, 192, 45),
+                              padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
+                              child: Text(
+                                '\$248',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                                child: Text(
+                                  'Add to cart',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
